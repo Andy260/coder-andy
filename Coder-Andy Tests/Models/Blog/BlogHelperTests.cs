28455c755d55240dfb538a16fe2cebd212d27b82
Andrew Barbour <andrew.barbour1@gmail.com>
2019-02-22 16:37:17 +1100

    Implemented BlogHelper class for various helper functions within the blog backend, and NameToLink() function to convert strings to URL safe names

---
+++ Coder-Andy Tests/Models/Blog/BlogHelperTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CoderAndy.Models.Blog.Tests
+{
+    [TestFixture]
+    public class BlogHelperTests
+    {
+        #region Test Case Sources
+
+        private class NameToLinkData
+        {
+            private static readonly char[] _validCharacters =
+            {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+            };
+
+            public static IEnumerable<string> InvalidCharacters
+            {
+                get
+                {
+                    List<string> invalidASCIIChars = new List<string>(char.MaxValue);
+                    for (int i = char.MinValue; i < 128; i++)
+                    {
+                        char c = Convert.ToChar(i);
+                        if (!char.IsControl(c) &&
+                            !_validCharacters.Contains(c))
+                        {
+                            invalidASCIIChars.Add(c.ToString());
+                        }
+                    }
+
+                    return invalidASCIIChars;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Function Tests
+
+        [Test]
+        [Category("Function Test")]
+        [Description("Tests NameToLinkName() function with expected input")]
+        [TestCase("Test Name", "test-name")]
+        [TestCase("Test  Name", "test-name")]
+        [TestCase(" Test Name ", "test-name")]
+        public void NameToLinkName(string a_name, string a_expectedLink)
+        {
+            Assert.AreEqual(a_expectedLink, BlogHelper.NameToLinkName(a_name));
+        }
+
+        [Test]
+        [Category("Function Test")]
+        [Description("Tests NameToLinkName() function with a single invalid character")]
+        [TestCaseSource(typeof(NameToLinkData), "InvalidCharacters")]
+        public void NameToLinkName_InvalidInputCharacter(string a_name)
+        {
+            Assert.AreEqual("_", BlogHelper.NameToLinkName(a_name));
+        }
+
+        [Test]
+        [Category("Function Test")]
+        [Description("Tests NameToLinkName() function with the input \"Test {invalid-character} Name\"")]
+        [TestCaseSource(typeof(NameToLinkData), "InvalidCharacters")]
+        public void NameToLinkName_MixedValidAndInvalidInput(string a_invalidCharacter)
+        {
+            string name = string.Format("Test {0} Name", a_invalidCharacter);
+
+            Assert.AreEqual("test-name", BlogHelper.NameToLinkName(name));
+        }
+
+        #endregion
+    }
+}
---
+++ Coder-Andy/Models/Blog/BlogHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CoderAndy.Models.Blog
+{
+    public static class BlogHelper
+    {
+        #region Public Functions
+
+        /// <summary>
+        /// Converts a name to a URL safe name
+        /// </summary>
+        /// <param name="a_name">Name to convert</param>
+        /// <returns>URL Safe name</returns>
+        public static string NameToLinkName(string a_name)
+        {
+            a_name = a_name.ToLower();
+
+            // Remove unsafe URL characters
+            a_name = new Regex("[^a-zA-Z0-9 ]").Replace(a_name, "");
+
+            // Replace white-space with dashes, so output has the equivalent 
+            // format: " Some Name " to "some-Name"
+            string[] splitLink = a_name.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            a_name = string.Join("-", splitLink);
+
+            // Ensure we don't return an empty string
+            if (string.IsNullOrEmpty(a_name))
+            {
+                a_name = "_";
+            }
+
+            return a_name;
+        }
+
+        #endregion
+    }
+}
